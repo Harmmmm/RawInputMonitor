6471b9c446a55d92c30b9996b949927fb3c2fcc6
Harm <git@harm.io>
2020-10-11 22:21:28 +0200

    1.0

---
+++ RawInputMonitor/MainWindow.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace RawInputMonitor
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.table = new System.Windows.Forms.DataGridView();
+            this.lblDevices = new System.Windows.Forms.Label();
+            this.lblEvents = new System.Windows.Forms.Label();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.chkEnabled = new System.Windows.Forms.CheckBox();
+            this.chkPosition = new System.Windows.Forms.CheckBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.Handle = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DeviceType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.VendorId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProductId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProductName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ManufacturerName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Mode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RawX = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RawY = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CalcX = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CalcY = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FancyName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Path = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.table)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // table
+            // 
+            this.table.AllowUserToResizeRows = false;
+            this.table.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.table.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Handle,
+            this.DeviceType,
+            this.VendorId,
+            this.ProductId,
+            this.ProductName,
+            this.ManufacturerName,
+            this.Mode,
+            this.RawX,
+            this.RawY,
+            this.CalcX,
+            this.CalcY,
+            this.FancyName,
+            this.Path});
+            this.table.Location = new System.Drawing.Point(3, 18);
+            this.table.Name = "table";
+            this.table.RowHeadersVisible = false;
+            this.table.Size = new System.Drawing.Size(1063, 249);
+            this.table.TabIndex = 0;
+            // 
+            // lblDevices
+            // 
+            this.lblDevices.AutoSize = true;
+            this.lblDevices.Location = new System.Drawing.Point(3, 2);
+            this.lblDevices.Name = "lblDevices";
+            this.lblDevices.Size = new System.Drawing.Size(49, 13);
+            this.lblDevices.TabIndex = 1;
+            this.lblDevices.Text = "Devices:";
+            // 
+            // lblEvents
+            // 
+            this.lblEvents.AutoSize = true;
+            this.lblEvents.Location = new System.Drawing.Point(3, 2);
+            this.lblEvents.Name = "lblEvents";
+            this.lblEvents.Size = new System.Drawing.Size(43, 13);
+            this.lblEvents.TabIndex = 2;
+            this.lblEvents.Text = "Events:";
+            // 
+            // txtLog
+            // 
+            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLog.Location = new System.Drawing.Point(3, 288);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(1063, 250);
+            this.txtLog.TabIndex = 3;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(870, 2);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(199, 13);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "github.com/Harmmmm/RawInputMonitor";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtLog, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.table, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 5);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1069, 541);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblDevices);
+            this.panel1.Controls.Add(this.linkLabel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1069, 15);
+            this.panel1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnClear);
+            this.panel2.Controls.Add(this.chkPosition);
+            this.panel2.Controls.Add(this.chkEnabled);
+            this.panel2.Controls.Add(this.lblEvents);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 270);
+            this.panel2.Margin = new System.Windows.Forms.Padding(0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1069, 15);
+            this.panel2.TabIndex = 2;
+            // 
+            // chkEnabled
+            // 
+            this.chkEnabled.AutoSize = true;
+            this.chkEnabled.Checked = true;
+            this.chkEnabled.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkEnabled.Location = new System.Drawing.Point(93, 1);
+            this.chkEnabled.Name = "chkEnabled";
+            this.chkEnabled.Size = new System.Drawing.Size(65, 17);
+            this.chkEnabled.TabIndex = 3;
+            this.chkEnabled.Text = "Enabled";
+            this.chkEnabled.UseVisualStyleBackColor = true;
+            // 
+            // chkPosition
+            // 
+            this.chkPosition.AutoSize = true;
+            this.chkPosition.Location = new System.Drawing.Point(164, 1);
+            this.chkPosition.Name = "chkPosition";
+            this.chkPosition.Size = new System.Drawing.Size(166, 17);
+            this.chkPosition.TabIndex = 4;
+            this.chkPosition.Text = "Show position change events";
+            this.chkPosition.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(350, -4);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(0);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Handle
+            // 
+            this.Handle.HeaderText = "Handle";
+            this.Handle.Name = "Handle";
+            this.Handle.ReadOnly = true;
+            // 
+            // DeviceType
+            // 
+            this.DeviceType.HeaderText = "DeviceType";
+            this.DeviceType.Name = "DeviceType";
+            this.DeviceType.ReadOnly = true;
+            // 
+            // VendorId
+            // 
+            this.VendorId.HeaderText = "VendorId";
+            this.VendorId.Name = "VendorId";
+            this.VendorId.ReadOnly = true;
+            // 
+            // ProductId
+            // 
+            this.ProductId.HeaderText = "ProductId";
+            this.ProductId.Name = "ProductId";
+            this.ProductId.ReadOnly = true;
+            // 
+            // ProductName
+            // 
+            this.ProductName.HeaderText = "ProductName";
+            this.ProductName.Name = "ProductName";
+            this.ProductName.ReadOnly = true;
+            // 
+            // ManufacturerName
+            // 
+            this.ManufacturerName.HeaderText = "ManufacturerName";
+            this.ManufacturerName.Name = "ManufacturerName";
+            this.ManufacturerName.ReadOnly = true;
+            // 
+            // Mode
+            // 
+            this.Mode.HeaderText = "Mode";
+            this.Mode.Name = "Mode";
+            this.Mode.ReadOnly = true;
+            // 
+            // RawX
+            // 
+            this.RawX.HeaderText = "RawX";
+            this.RawX.Name = "RawX";
+            this.RawX.ReadOnly = true;
+            // 
+            // RawY
+            // 
+            this.RawY.HeaderText = "RawY";
+            this.RawY.Name = "RawY";
+            this.RawY.ReadOnly = true;
+            // 
+            // CalcX
+            // 
+            this.CalcX.HeaderText = "CalcX";
+            this.CalcX.Name = "CalcX";
+            this.CalcX.ReadOnly = true;
+            // 
+            // CalcY
+            // 
+            this.CalcY.HeaderText = "CalcY";
+            this.CalcY.Name = "CalcY";
+            this.CalcY.ReadOnly = true;
+            // 
+            // FancyName
+            // 
+            this.FancyName.HeaderText = "FancyName";
+            this.FancyName.Name = "FancyName";
+            // 
+            // Path
+            // 
+            this.Path.HeaderText = "Path";
+            this.Path.Name = "Path";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1093, 558);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "MainWindow";
+            this.ShowIcon = false;
+            this.Text = "Raw Input Monitor";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainWindow_FormClosed);
+            this.Shown += new System.EventHandler(this.MainWindow_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.table)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView table;
+        private System.Windows.Forms.Label lblDevices;
+        private System.Windows.Forms.Label lblEvents;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.CheckBox chkEnabled;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.CheckBox chkPosition;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Handle;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DeviceType;
+        private System.Windows.Forms.DataGridViewTextBoxColumn VendorId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProductId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProductName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ManufacturerName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Mode;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RawX;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RawY;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CalcX;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CalcY;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FancyName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Path;
+    }
+}
+
---
+++ RawInputMonitor/MainWindow.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Forms;
+using Linearstar.Windows.RawInput;
+using Linearstar.Windows.RawInput.Native;
+
+namespace RawInputMonitor
+{
+    public partial class MainWindow : Form
+    {
+        private int screenWidth = (int)SystemParameters.PrimaryScreenWidth;
+        private int screenHeight = (int)SystemParameters.PrimaryScreenHeight;
+        private const int WM_INPUT = 0x00FF;
+        private const int WM_DEVICECHANGE = 537;
+        private const int DBT_DEVNODES_CHANGED = 0x0007;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Log(string s)
+        {
+            txtLog.AppendText(s);
+            txtLog.AppendText(Environment.NewLine);
+        }
+
+        private string GetFancyDeviceName(RawInputDevice device)
+        {
+            string fancyName = "";
+
+            if (device == null)
+                return "Unknown device (NULL)";
+
+            try
+            {
+                // Aimtrak
+                if (device.VendorId == 0xD209 && device.ProductId >= 0x1601 && device.ProductId <= 0x1608)
+                {
+                    fancyName = String.Format("Ultimarc AimTrak #{0}", device.ProductId - 0x1600);
+                }
+                // Sinden
+                else if (device.VendorId == 0x16C0)
+                {
+                    if (device.ProductId == 0x0F01)
+                        fancyName = "Sinden Lightgun Blue";
+                    else if (device.ProductId == 0x0F02)
+                        fancyName = "Sinden Lightgun Red";
+                    else if (device.ProductId == 0x0F38)
+                        fancyName = "Sinden Lightgun Black";
+                    else if (device.ProductId == 0x0F39)
+                        fancyName = "Sinden Lightgun Player 2";
+                }
+            }
+            catch (Exception e)
+            {
+                Log($"Exception {e}");
+            }
+
+            try
+            {
+                // Other
+                if (fancyName == "")
+                {
+                    string manufacturer = device.ManufacturerName.Trim();
+
+                    if (manufacturer == "(Standard keyboards)" || device.ProductName.Contains(manufacturer))
+                        manufacturer = "";
+
+                    fancyName = String.Format("{0} {1}", manufacturer, device.ProductName.Trim()).Trim();
+                }
+            }
+            catch (Exception e)
+            {
+                Log($"Exception {e}");
+                return "Unknown device (nameless)";
+            }
+
+            return fancyName;
+        }
+
+        private void UpdateDeviceList()
+        {
+            var devices = RawInputDevice.GetDevices();
+            var keyboards = devices.OfType<RawInputKeyboard>();
+            var mice = devices.OfType<RawInputMouse>();
+
+            table.Rows.Clear();
+
+            foreach (var device in mice)
+            {
+                string[] row = { RawInputDeviceHandle.GetRawValue(device.Handle).ToString("X8"), device.DeviceType.ToString(), device.VendorId.ToString("X4"), device.ProductId.ToString("X4"), device.ProductName.ToString(), device.ManufacturerName.ToString(), "?", "?", "?", (screenWidth / 2).ToString(), (screenHeight / 2).ToString(), GetFancyDeviceName(device), device.DevicePath.ToString() };
+                table.Rows.Add(row);
+            }
+
+            foreach (var device in keyboards)
+            {
+                string[] row = { RawInputDeviceHandle.GetRawValue(device.Handle).ToString("X8"), device.DeviceType.ToString(), device.VendorId.ToString("X4"), device.ProductId.ToString("X4"), device.ProductName.ToString(), device.ManufacturerName.ToString(), "?", "?", "?", (screenWidth / 2).ToString(), (screenHeight / 2).ToString(), GetFancyDeviceName(device), device.DevicePath.ToString() };
+                table.Rows.Add(row);
+            }
+
+            table.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            // Raw input event
+            if (m.Msg == WM_INPUT)
+            {
+                var data = RawInputData.FromHandle(m.LParam);
+
+                if (data == null)
+                {
+                    Log("RawInputData NULL!");
+                    base.WndProc(ref m);
+                    return;
+                }
+
+                // Device type
+                switch (data)
+                {
+                    case RawInputMouseData mouse:
+                        var handleM = RawInputDeviceHandle.GetRawValue(mouse.Device.Handle).ToString("X8");
+                        var nameM = GetFancyDeviceName(mouse.Device);
+                        var mode = mouse.Mouse.Flags;
+                        var x = mouse.Mouse.LastX;
+                        var y = mouse.Mouse.LastY;
+
+                        for (int i = 0; i < table.Rows.Count; i++)
+                        {
+                            if ((string)(table.Rows[i].Cells[0].Value) == handleM)
+                            {
+                                bool resize = false;
+
+                                if (table.Rows[i].Cells[6].Value.ToString() != mode.ToString())
+                                    resize = true;
+
+                                table.Rows[i].Cells[6].Value = mode;
+                                table.Rows[i].Cells[7].Value = x;
+                                table.Rows[i].Cells[8].Value = y;
+
+                                if (mode.HasFlag(RawMouseFlags.MoveRelative))
+                                {
+                                    // Super advanced position determination
+                                    table.Rows[i].Cells[9].Value = Math.Min(Math.Max(Int32.Parse(table.Rows[i].Cells[9].Value.ToString()) + x, 0), screenWidth);
+                                    table.Rows[i].Cells[10].Value = Math.Min(Math.Max(Int32.Parse(table.Rows[i].Cells[10].Value.ToString()) + y, 0), screenHeight);
+                                }
+                                else if (mode.HasFlag(RawMouseFlags.MoveAbsolute))
+                                {
+                                    table.Rows[i].Cells[9].Value = Math.Round((float)x / (float)0xFFFF * (float)screenWidth).ToString();
+                                    table.Rows[i].Cells[10].Value = Math.Round((float)y / (float)0xFFFF * (float)screenHeight).ToString();
+
+                                }
+
+                                if (resize)
+                                    table.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+
+                                break;
+                            }
+                        }
+
+                        // Log event?
+                        if (chkEnabled.Checked)
+                        {
+                            if (mouse.Mouse.Buttons != RawMouseButtonFlags.None || (mouse.Mouse.Buttons == RawMouseButtonFlags.None && chkPosition.Checked))
+                            {
+                                Log($"Handle: {handleM:X8}\tName: {nameM}\tX: {mouse.Mouse.LastX}\tY: {mouse.Mouse.LastY}\tFlags: {mouse.Mouse.Flags}\tButtons: {mouse.Mouse.Buttons}\tData: {mouse.Mouse.ButtonData}");
+                            }
+                        }
+                        
+                        break;
+                    case RawInputKeyboardData keyboard:
+                        var handleK = RawInputDeviceHandle.GetRawValue(keyboard.Device.Handle).ToString("X8");
+                        var nameK = GetFancyDeviceName(keyboard.Device);
+
+                        // Log event?
+                        if (chkEnabled.Checked)
+                            Log($"Handle: {handleK:X8}\tName: {nameK}\tKeyCode: {keyboard.Keyboard.VirutalKey}\tScanCode: {keyboard.Keyboard.ScanCode}\tKey: {(Keys)keyboard.Keyboard.VirutalKey}\tFlags: {keyboard.Keyboard.Flags}");
+
+                        break;
+                    case RawInputHidData hid:
+                        Log(hid.Hid.ToString());
+                        break;
+                }
+            }
+            // Device added or removed event
+            else if (m.Msg == WM_DEVICECHANGE)
+            {
+                if ((int)m.WParam == DBT_DEVNODES_CHANGED)
+                    UpdateDeviceList();
+            }
+            
+            base.WndProc(ref m);
+        }
+
+        private void MainWindow_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            RawInputDevice.UnregisterDevice(HidUsageAndPage.Mouse);
+        }
+
+        private void MainWindow_Shown(object sender, EventArgs e)
+        {
+            // Update title
+            Version v = Assembly.GetExecutingAssembly().GetName().Version;
+            this.Text = $"Raw Input Monitor V{v.Major}.{v.Minor}";
+
+            // Get devices
+            UpdateDeviceList();
+
+            // Get our handle
+            int pid = Process.GetCurrentProcess().Id;
+            Process proc = Process.GetProcessById(pid);
+            var wHandle = proc.MainWindowHandle;
+
+            // Register to raw input events
+            RawInputDevice.RegisterDevice(HidUsageAndPage.Mouse, RawInputDeviceFlags.InputSink, wHandle);
+            RawInputDevice.RegisterDevice(HidUsageAndPage.Keyboard, RawInputDeviceFlags.InputSink, wHandle);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/Harmmmm/RawInputMonitor");
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtLog.Clear();
+        }
+    }
+}
